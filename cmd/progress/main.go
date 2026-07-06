c60b0bfee924f271dd37875af2eb16e2990dc5af
Ruslan Sungatullin <r.a.sungatullin@gmail.com>
2026-07-06 10:41:18 +0300

    Фиксировать прерванный запуск исполнения

--- cmd/progress/main.go
+++ cmd/progress/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/rasungatullin/progress/internal/cli"
 )
 
 func main() {
-	if err := cli.NewRootCommand().Execute(); err != nil {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	if err := cli.NewRootCommand().ExecuteContext(ctx); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
